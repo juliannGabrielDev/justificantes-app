194443b6bf693042864fc0d38e20d7cf15fea074
juliannGabrielDev <julian.alejandro.gabriel@gmail.com>
2026-06-07 23:13:49 -0600

    feat(ui): :sparkles: refactor layout component to use RootLayout with theme support and font loading

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,79 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import '@/global.css';
+import { Geist_400Regular, Geist_700Bold } from '@expo-google-fonts/geist';
+import {
+	GeistMono_400Regular,
+	GeistMono_500Medium,
+	GeistMono_600SemiBold,
+	GeistMono_700Bold,
+} from '@expo-google-fonts/geist-mono';
+import { Pacifico_400Regular } from '@expo-google-fonts/pacifico';
+import { useFonts } from 'expo-font';
+import {
+	DarkTheme,
+	DefaultTheme,
+	SplashScreen,
+	Stack,
+	ThemeProvider,
+} from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
+SplashScreen.preventAutoHideAsync();
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+const MyDefaultTheme = {
+	...DefaultTheme,
+	colors: {
+		...DefaultTheme.colors,
+		background: '#f3f3ed',
+	},
+};
+
+const MyDarkTheme = {
+	...DarkTheme,
+	colors: {
+		...DarkTheme.colors,
+		background: '#131313',
+	},
+};
+
+export default function RootLayout() {
+	const colorScheme = useColorScheme();
+	const [fontsLoaded, error] = useFonts({
+		'Geist-Regular': Geist_400Regular,
+		'Geist-Bold': Geist_700Bold,
+		'Geist-Mono': GeistMono_400Regular,
+		'Geist-Mono-Medium': GeistMono_500Medium,
+		'Geist-Mono-SemiBold': GeistMono_600SemiBold,
+		'Geist-Mono-Bold': GeistMono_700Bold,
+		Pacifico: Pacifico_400Regular,
+	});
+
+	useEffect(() => {
+		if (error) throw error;
+
+		if (fontsLoaded) {
+			SplashScreen.hideAsync();
+		}
+	}, [fontsLoaded, error]);
+
+	if (!fontsLoaded) {
+		return null;
+	}
+
+	return (
+		<GestureHandlerRootView style={{ flex: 1 }}>
+			<ThemeProvider
+				value={colorScheme === 'dark' ? MyDarkTheme : MyDefaultTheme}
+			>
+				<Stack screenOptions={{ headerShown: false }} />
+				<StatusBar
+					style={colorScheme === 'dark' ? 'light' : 'dark'}
+					animated
+				/>
+			</ThemeProvider>
+		</GestureHandlerRootView>
+	);
 }
